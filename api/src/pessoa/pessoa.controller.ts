cea50063daf44d270f661eb2da9f020776fafba9
Matuges <d4rkdra@gmail.com>
2026-08-14 15:17:30 -0300

    Service WIP

--- api/src/pessoa/pessoa.controller.ts
+++ api/src/pessoa/pessoa.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PessoaService } from './pessoa.service';
+import { CreatePessoaDto } from './dto/create-pessoa.dto';
 
 @Controller('pessoa')
-export class PessoaController {}
+export class PessoaController {
+    constructor (private readonly pessoaService: PessoaService) {}
+
+    @Post()
+    create(@Body() dto: CreatePessoaDto) {
+
+    }
+
+    @Get()
+    findAll() {
+
+    }
+
+    
+
+}
--- api/src/pessoa/pessoa.service.ts
+++ api/src/pessoa/pessoa.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePessoaDto } from './dto/create-pessoa.dto';
 
 @Injectable()
-export class PessoaService {}
+export class PessoaService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    create(dto: CreatePessoaDto) {
+        this.prisma.pessoa.create(
+            
+        )
+    }
+
+    findAll() {
+        
+    }
+}
