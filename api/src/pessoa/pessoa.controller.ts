6259591aba462eb37f5e965a6b09dfbc431b66f5
Matuges <d4rkdra@gmail.com>
2026-08-15 01:33:58 -0300

    CRUD resources finished

--- api/src/pessoa/pessoa.controller.ts
+++ api/src/pessoa/pessoa.controller.ts
@@ -1,20 +1,37 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { PessoaService } from './pessoa.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
+import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 
 @Controller('pessoa')
 export class PessoaController {
     constructor (private readonly pessoaService: PessoaService) {}
 
     @Post()
-    create(@Body() dto: CreatePessoaDto) {
-
+    async create(@Body() dto: CreatePessoaDto) {
+        return this.pessoaService.create(dto)
     }
 
     @Get()
-    findAll() {
-
+    async findAll() {
+            return this.pessoaService.findAll()
     }
+
+    @Get(':id') 
+    async findUnique(@Param('id', ParseIntPipe) id: number) {
+            return this.pessoaService.findUnique(id)
+        }
+
+    @Patch(':id')
+    async update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdatePessoaDto) {
+        return this.pessoaService.update(id, dto)
+    }
+
+    @Delete(':id')
+    async remove(@Param('id', ParseIntPipe) id: number) {
+        return this.pessoaService.remove(id)
+    }
+    
 
     
 
--- api/src/pessoa/pessoa.service.ts
+++ api/src/pessoa/pessoa.service.ts
@@ -1,18 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
+import { UpdatePessoaDto } from './dto/update-pessoa.dto';
 
 @Injectable()
 export class PessoaService {
     constructor(private readonly prisma: PrismaService) {}
 
-    create(dto: CreatePessoaDto) {
-        this.prisma.pessoa.create(
-            
+    async create(dto: CreatePessoaDto) {
+        return this.prisma.pessoa.create({
+            data: {
+                nome: dto.nome
+            }
+        }
         )
     }
 
-    findAll() {
-        
+    async findAll() {
+        return this.prisma.pessoa.findMany()
+    }
+
+    async findUnique(id: number) {
+        return this.prisma.pessoa.findUnique({
+            where: {
+                id: id
+            }
+        })
+    }
+
+    async update(id: number, dto: UpdatePessoaDto) {
+        return this.prisma.pessoa.update({
+            data: {
+                nome: dto.nome
+            },
+            where: {
+                id: id
+            }
+        })
+    }
+
+    async remove(id: number) {
+        return this.prisma.pessoa.delete({
+            where: {
+                id: id
+            }
+        })
     }
 }
